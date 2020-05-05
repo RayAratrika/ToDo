105cdac63dd37662ab02b107dce99f74c70a39e1
Aratrika Ray <aratrika.ray1999@gmail.com>
2020-05-06 02:57:55 +0530

    Updates

---
+++ precache-manifest.14c03c96f8f41ddef024891918736843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d929763f8631d2e1aa3976f98f8f09",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "9e630608128dbcb65c84",
+    "url": "/ToDo/static/css/main.9dbec377.chunk.css"
+  },
+  {
+    "revision": "27f934468f1773618df9",
+    "url": "/ToDo/static/js/2.af2fe22f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo/static/js/2.af2fe22f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e630608128dbcb65c84",
+    "url": "/ToDo/static/js/main.ab19da4e.chunk.js"
+  },
+  {
+    "revision": "c8f32f532e4d985b55c6",
+    "url": "/ToDo/static/js/runtime-main.27b8da09.js"
+  }
+]);
